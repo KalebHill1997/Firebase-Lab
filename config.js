4fb76639361be16b623eae935f2ea64b16a427a9
Kaleb Hill <79117151+KalebHill1997@users.noreply.github.com>
2021-04-06 14:41:45 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyBOQhJom-C-wXWr6DHXEJV_UIXs2ZnseS8",
+  authDomain: "my-first-firebase-proj-c4e34.firebaseapp.com",
+  databaseURL:
+    "https://my-first-firebase-proj-c4e34-default-rtdb.firebaseio.com",
+  projectId: "my-first-firebase-proj-c4e34",
+  storageBucket: "my-first-firebase-proj-c4e34.appspot.com",
+  messagingSenderId: "1057663102957",
+};
