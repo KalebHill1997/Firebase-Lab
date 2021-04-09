c7ff14c9a6ea35b261dbda41ce40b77cec857c13
Kaleb Hill <79117151+KalebHill1997@users.noreply.github.com>
2021-04-09 08:07:37 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
-  apiKey: "AIzaSyBOQhJom-C-wXWr6DHXEJV_UIXs2ZnseS8",
+  apiKey: "",
   authDomain: "my-first-firebase-proj-c4e34.firebaseapp.com",
   databaseURL:
     "https://my-first-firebase-proj-c4e34-default-rtdb.firebaseio.com",
